d9dcfe0e4e08f646671a07c50af5a675fd5f3e72
ESHAN PRASAD BAIKAN <106946041+Eshanprasad@users.noreply.github.com>
2024-02-22 21:33:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+This code finds the two smallest elements in the list in single iteration
+"""
+
+arr = [5,3,4,5,2,6, 1, 1]
+first = 10000   #assuming 10000 is max number
+second = 10000  #assuming 10000 is max number
+for i in range(0, len(arr)):
+    if arr[i] < first:
+        second = first
+        first = arr[i]
+    elif (arr[i] < second):
+        second = arr[i]
