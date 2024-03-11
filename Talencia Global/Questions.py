bfbe9879679d7d0775ce56868402bc56575784f3
ESHAN PRASAD BAIKAN <106946041+Eshanprasad@users.noreply.github.com>
2024-03-12 05:12:53 +0530

    Create Questions.py

---
+++ Talencia Global/Questions.py
@@ -0,0 +1,32 @@
+"""
+Question 1:
+
+A vendor offers software services to a client. Each resource is billed at some dollar rate per hour. The total cost of the project for the client is therefore, the total number of hours contributed by all the vendor resources * the dollar rate / hour. There are however some variants.
+
+a. The vendor might have purchased hardware/infrastructure or software licenses needed for the project.  
+b. The vendor might have utilized external consultants for the project. 
+c. The client looks at the vendor as a one stop solution and hence external resources employed by the vendor need to be paid by the vendor. 
+d. It might however be possible that the vendor’s hardware and software purchases are borne by the client. In this case, the client pays the vendor 30% of the hardware/infrastructure costs. In case of software licenses, the client pays the vendor 50% of the cost, if they are commonly available and used, or 100% if the software is infrequently used or is proprietary client technology. 
+e. The external consultants employed by the vendor will come at a dollar rate per hour. 
+f. Accept the suitable inputs and display the profits / loss realized by the vendor.
+
+
+
+Question 2:
+
+Mahesh is a farmer and owns 80 acres of land. His land is equally divided into 5 segments. He grows tomatoes in the 1st segment, potatoes in the 2nd segment, cabbage in the 3rd segment, sunflower in the 4th segment and sugarcane in the 5th segment.He is converting his land from chemical-driven farming to chemical-free farming. Mahesh starts with 
+the conversion of vegetables into chemical-free produce. He spends the first 6 months doing the same. He then converts the sunflower land bank into chemical-free farming. This takes him another 4 months. Finally, he converts sugarcane into chemical-free farming over the next 4 months.
+He gets a yield of the following for tomatoes. 30% of his tomato land gives him 10 tonne yield per acre. The remaining 70% of his tomato land gives him 12 tonnes yield per acre. The selling price of tomato is Rs. 7 per Kg.
+ 
+The yield of potatoes is 10 tonnes per acre. He sells each kg at Rs. 20.
+The yield of cabbage is 14 tonnes per acre. He sells each kg at Rs. 24.
+The yield of sunflowers is 0.7 tonnes per acre. He sells each kg at Rs. 200.
+The yield of sugarcane is 45 tonnes per acre. He sells each tonne at Rs. 4,000.
+
+All the crops are sowed at the same time. Mahesh gets the above yield at the above-mentioned rate 
+in one crop cycle across his entire land of 80 acres.
+What is 
+a. The overall sales achieved by Mahesh from the 80 acres of land.
+b. Sales realisation from chemical-free farming at the end of 11 months
+
+"""
