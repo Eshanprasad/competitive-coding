cca23948f5f1a94ae57e012a58118889f0067204
ESHAN PRASAD BAIKAN <106946041+Eshanprasad@users.noreply.github.com>
2024-04-21 17:44:30 +0530

    Update bmi_calculator.py

--- Accenture/Primers/bmi_calculator.py
+++ Accenture/Primers/bmi_calculator.py
@@ -1,3 +1,77 @@
+"""
+Objective:
+
+To work with Control structures
+
+
+
+Problem Description:
+
+Program in Python to compute the BMI of a person and display the  risk associated with it by entering the height in 'm' and weight in' kg'. Refer the following table and code accordingly.
+
+Guidelines:
+
+To calculate the BMI apply the formula:  BMI = weight(kg)/( height(m)*height(m) ).    
+Result must be adjusted to one decimal place. 
+When the height or weight is entered as a negative number or  zero, then display the message "Provide a valid input" and stop the program.
+
+
+BMI
+
+Risk
+
+27.5 and above
+
+High Risk
+
+23 - 27.4
+
+Moderate Risk
+
+18.5 - 22.9
+
+Low Risk
+
+Below 18.5
+
+Risk of nutritional deficiency diseases
+
+
+
+
+Sample Input 1:
+Enter the weight of the person(kg):85
+
+Enter the height of the person(m):1.75
+
+Sample Output 1:
+
+Your BMI is 27.8 (High Risk).
+
+Sample Input 2:
+
+Enter the weight of the person(kg):0
+
+Enter the height of the person(m):1.58
+
+Sample Output 2:
+
+Provide a valid input
+
+
+
+Sample Input 3:
+
+Enter the weight of the person(kg):80
+
+Enter the height of the person(m):-1
+
+Sample Output 3:
+
+Provide a valid input
+
+"""
+
 weight = int(input("Enter the weight of the person(kg):"))
 height = float(input("Enter the height of the person(m):"))
 if weight<=0 or height<=0:
